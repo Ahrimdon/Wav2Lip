58715607a7d9b3226e9867ad9d86177f92611a5c
Ahrimdon <mrcodblackops10@gmail.com>
2023-09-29 21:59:00 -0400

    Changed batch file

--- wav2lip.bat
+++ wav2lip.bat
@@ -1,3 +1,3 @@
 call venv\scripts\activate.bat
 
-python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "RhinoLipFootage720p25fps.mp4" --audio "beaner_sex_slave.wav" --pads 0 10 0 0 --resize_factor 1
+python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "InputVideo720p30fps.mp4" --audio "InputAudio.wav" --pads 0 10 0 0 --resize_factor 1
